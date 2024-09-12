338a3802f8d3d67ddd2d917ce7f1a33c2d72e427
YudanMaulana <yudanmaulana21@gmail.com>
2024-09-12 18:45:06 +0700

    variable

---
+++ variable.py
@@ -0,0 +1,25 @@
+a = "yudan"
+b = 21
+c = 0.21
+d = True
+
+# Casting Variable
+e = str(1) # will be "1"
+f = int(1) # will be 1
+g = float(1) # will be 1.0
+
+#type check with type() in print
+print(type(a)) # output will be str (string type)
+
+# rules for create name variable
+camelCase = "with camel case style"
+PascalCase = "with pascal case style"
+snake_case = "with snake case style"
+
+#can be use  single or double quotes
+single = 'single quotes'
+double = "double quotes"
+
+# case sensitive
+h = 21
+H = "twenty one"  #H will not overwrite a
